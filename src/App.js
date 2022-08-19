91875142cc7b77eafa4ded1e709627be50cc1852
michaelphillips4 <31538839+michaelphillips4@users.noreply.github.com>
2022-08-19 15:22:03 +0100

    Added better mobile support

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 
 import Music from "./Components/Music/Music.js";
-import Video from "./Components/Video/Video.js";
 
 function App() {
   return (
     <>
       <Music />
-      <Video />
+ 
     </>
   );
 }
